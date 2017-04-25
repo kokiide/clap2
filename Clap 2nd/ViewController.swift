817f347b2f1427af4f86924cd69e137358559f0e
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-04-25 19:05:03 +0900

    1st

--- Clap 2nd/ViewController.swift
+++ Clap 2nd/ViewController.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var audioPlayer: AVAudioPlayer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //再生する音源のURLを生成
+        let soundFilePath = Bundle.main.path(forResource: "clap", ofType: "wav")!
+        let fileURL = URL(fileURLWithPath: soundFilePath)
+        
+        //AVAudioPlayer　のインスタンス化
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
+        } catch {
+            print("音楽ファイルが読み込めませんでした")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playbutton(){
+    audioPlayer.play()
     }
 
 
